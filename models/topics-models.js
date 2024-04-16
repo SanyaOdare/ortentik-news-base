8dbae141aa5cf1d707f7c2611ef927e873bdcd42
SanyaOdare <sanyaodare@gmail.com>
2024-04-16 12:06:34 +0100

    get connection path correctly

---
+++ models/topics-models.js
@@ -0,0 +1,13 @@
+const db = require('../db/connection');
+
+function fetchTopics() {
+  return db
+    .query(`SELECT * FROM topics`)
+    .then((data) => {
+    const topics = data.rows;
+    console.log(topics, '<---- in database');
+    return topics;    
+  });
+}
+
+module.exports = { fetchTopics };
